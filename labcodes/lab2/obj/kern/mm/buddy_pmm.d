f2fa57a5370a3b527b297ec2e430d6eb847bc966
yslsh <2076414960@qq.com>
2025-10-20 01:04:34 +0800

    Add files via upload

---
+++ labcodes/lab2/obj/kern/mm/buddy_pmm.d
@@ -0,0 +1,4 @@
+obj/kern/mm/buddy_pmm.o obj/kern/mm/buddy_pmm.d: kern/mm/buddy_pmm.c \
+ kern/mm/pmm.h kern/debug/assert.h libs/defs.h kern/mm/memlayout.h \
+ libs/list.h kern/mm/mmu.h libs/riscv.h kern/mm/buddy_pmm.h libs/string.h \
+ libs/stdio.h libs/stdarg.h
---
+++ labcodes/lab2/obj/kern/mm/slub.d
@@ -0,0 +1,4 @@
+obj/kern/mm/slub.o obj/kern/mm/slub.d: kern/mm/slub.c kern/mm/pmm.h \
+ kern/debug/assert.h libs/defs.h kern/mm/memlayout.h libs/list.h \
+ kern/mm/mmu.h libs/riscv.h libs/stdio.h libs/stdarg.h libs/string.h \
+ kern/mm/buddy_pmm.h kern/mm/slub.h
